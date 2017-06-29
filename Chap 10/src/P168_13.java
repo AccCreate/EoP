d551e42f8f1fc854e506851d7048e770ab71d4b2
Dong Hyeon Seo <ds3457@columbia.edu>
2017-06-28 20:30:19 -0400

    Convert to IntelliJ accepted module system

--- Chap 10/src/P168_13.java
+++ Chap 10/src/P168_13.java
@@ -1,10 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class P168_13 {
     
     public static void main(String[] args) {
-	
+        List<Integer> preOrder = new ArrayList<Integer>();
+        preOrder.add(1);
+        preOrder.add(2);
+        preOrder.add(3);
+        preOrder.add(4);
+        preOrder.add(5);
+        getBinaryTree(preOrder);
     }
 
-    public static BinaryTreeNode getBinaryTree(LinkedList<Integer> list){
-        
+    public static BinaryTree getBinaryTree (List<Integer> LList){
+
+
+        return new BinaryTree(12);
     }
+
 }
